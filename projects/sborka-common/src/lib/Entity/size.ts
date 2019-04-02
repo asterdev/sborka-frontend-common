63375d54c4d17ea21e75a6c65e46f820123e2b38
Arthur Ayvazov <armancho1@gmail.com>
2019-04-03 06:51:11 +0800

    Order entity.

--- projects/sborka-common/src/lib/Entity/size.ts
+++ projects/sborka-common/src/lib/Entity/size.ts
@@ -7,14 +7,14 @@ export class Size implements ObjectWithId {
     height: number;
     diameter: number;
 
-    summary() {
-        if (this.length === undefined) {
+    static summary(size: Size) {
+        if (size.length === undefined) {
             return null;
         }
 
-        let description = `${this.length}x${this.width}x${this.height}`;
-        if (this.diameter) {
-            description += ` D${this.diameter}`;
+        let description = size.length + 'x' + size.width + 'x' + size.height;
+        if (size.diameter) {
+            description += ' D' + size.diameter;
         }
 
         return description;
