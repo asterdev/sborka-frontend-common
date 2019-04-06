23e752132e559caa0827a7089495184e01fc6f38
ovsyanik.a <ovsyanik@vsk.ru>
2019-04-06 13:27:12 +0300

    LegalType correction

--- projects/sborka-common/src/lib/Entity/legal-type.ts
+++ projects/sborka-common/src/lib/Entity/legal-type.ts
@@ -2,6 +2,7 @@ import {ObjectWithId} from './object-with-id';
 import {LegalTypeEnum} from './legal-type-enum';
 
 export class LegalType implements ObjectWithId {
-  id: LegalTypeEnum;
+  id: number;
+  type: LegalTypeEnum;
   title: string;
 }
