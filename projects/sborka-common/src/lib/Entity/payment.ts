3869cb178b0239cc3731abd0fcdc0a3efd71ebd7
Arthur Ayvazov <armancho1@gmail.com>
2019-04-08 11:41:43 +0800

    Payment, PaymentType entities.

--- projects/sborka-common/src/lib/Entity/payment.ts
+++ projects/sborka-common/src/lib/Entity/payment.ts
@@ -1,8 +1,10 @@
 import {ObjectWithId} from './object-with-id';
 import {PaymentType} from './payment-type';
+import {User} from './user';
 
 export class Payment implements ObjectWithId {
   id: number;
+  user: User;
   date: string;
   sum: number;
   type: PaymentType;
