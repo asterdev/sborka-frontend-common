d6d4419cc511fbd0a440a4f582a43192524267a6
Arthur Ayvazov <armancho1@gmail.com>
2019-04-08 12:12:34 +0800

    Payment, PaymentType entities.

--- projects/sborka-common/src/lib/Entity/payment-type.ts
+++ projects/sborka-common/src/lib/Entity/payment-type.ts
@@ -3,4 +3,7 @@ import {ObjectWithId} from './object-with-id';
 export class PaymentType implements ObjectWithId {
   id: number;
   name: string;
+  shortName: string;
+  commission: number; // Комиссия за перевод
+  duration: string; // Время зачисления на баланс
 }
